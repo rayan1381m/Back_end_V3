35f1c7c22ec591fbe0d7459ec61789547360f72c
Git-Test <rayan13811381@gmail.com>
2024-03-02 00:58:41 +0330

    create post api for games and test it

--- game.js
+++ game.js
@@ -88,4 +88,49 @@ router.get("/name/:name", async (req, res) => {
   }
 });
 
+//post api
+// Function to create a new game with validation
+async function createGame(name, likes, comments, price) {
+  // Check if name and price are provided and price is not negative
+  if (!name || !price || price < 0) {
+    throw new Error(
+      "Invalid game data. Name and price are required, and price cannot be negative."
+    );
+  }
+
+  try {
+    await client.connect();
+
+    const result = await client.query(
+      "INSERT INTO games (name, likes, comments, price) VALUES ($1, $2, $3, $4) RETURNING *",
+      [name, likes, comments, price]
+    );
+    return result.rows[0];
+  } catch (error) {
+    console.error("Error querying PostgreSQL:", error);
+    throw new Error("Internal server error");
+  } finally {
+    await client.end();
+  }
+}
+
+//postman test: http://localhost:3000/games
+/*{
+    "name": "Elden Rings",
+    "likes": 0,
+    "comments": "ghalat kardm ghalat",
+    "price": 13.45
+} */
+router.post("/", async (req, res) => {
+  const { name, likes, comments, price } = req.body;
+
+  try {
+    const newGame = await createGame(name, likes, comments, price);
+    res.status(201).json(newGame);
+  } catch (error) {
+    console.error("Error:", error.message);
+    res.status(500).json({ message: error.message });
+  }
+});
+
 module.exports = router;
