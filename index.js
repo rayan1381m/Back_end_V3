a815b95790abaee38afe2e6265bec2bbfab61020
Git-Test <rayan13811381@gmail.com>
2024-03-01 21:12:54 +0330

    connected to data base

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
-const express = require("express");
-const app = express();
-const port = process.env.PORT || 3000;
+const { Client } = require('pg');
+require('dotenv').config();
 
-app.get("/", (req, res) => {
-  res.send("Hello, world!");
+const {
+    PGHOST,
+    PGDATABASE,
+    PGUSER,
+    PGPASSWORD,
+    PGPORT,
+    ENDPOINT_ID
+} = process.env;
+
+const client = new Client({
+    host: PGHOST,
+    port: PGPORT,
+    database: PGDATABASE,
+    user: PGUSER,
+    password: PGPASSWORD,
+    ssl: true, 
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+async function connectToDatabase() {
+    try {
+        await client.connect();
+        console.log('Connected to PostgreSQL database');
+    } catch (error) {
+        console.error('Error connecting to PostgreSQL database:', error);
+    }
+}
+
+async function getPgVersion() {
+    try {
+        const result = await client.query('SELECT version()');
+        console.log(result.rows[0]);
+    } catch (error) {
+        console.error('Error querying PostgreSQL:', error);
+    }
+}
+
+connectToDatabase();
+getPgVersion();
