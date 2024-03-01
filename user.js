e2b53141d6cfa9f7c0645bc5d536e55faa4aa613
Git-Test <rayan13811381@gmail.com>
2024-03-01 21:32:27 +0330

    get api by name

--- user.js
+++ user.js
@@ -36,11 +36,44 @@ async function getUserById(userId) {
     }
 }
 
+//postman test: http://localhost:3000/users/1
 router.get('/:id', async (req, res) => {
     const userId = req.params.id;
 
     try {
         const user = await getUserById(userId);
+
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        res.json(user);
+    } catch (error) {
+        console.error('Error:', error.message);
+        res.status(500).json({ message: error.message });
+    }
+});
+
+async function getUserByName(name) {
+    try {
+        await client.connect();
+
+        const result = await client.query('SELECT * FROM users WHERE name = $1', [name]);
+        return result.rows[0];
+    } catch (error) {
+        console.error('Error querying PostgreSQL:', error);
+        throw new Error('Internal server error');
+    } finally {
+        await client.end();
+    }
+}
+
+//postman test: http://localhost:3000/users/name/User2
+router.get('/name/:name', async (req, res) => {
+    const name = req.params.name;
+
+    try {
+        const user = await getUserByName(name);
 
         if (!user) {
             return res.status(404).json({ message: 'User not found' });
