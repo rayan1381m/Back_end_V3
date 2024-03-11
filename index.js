b2900e001ef46416acb2117c1511b1ca50b36627
Git-Test <rayan13811381@gmail.com>
2024-03-11 04:29:53 +0330

    create port 3001 and root directory of your back-end project

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const app = express();
 const cors = require("cors");
 const userRoutes = require('./user');
 const gameRoutes = require('./game.js');
-const bodyParser = require('body-parser');
 
 app.use(cors());
 
@@ -11,7 +10,7 @@ app.use(express.json());
 app.use('/users', userRoutes);
 app.use('/games', gameRoutes);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
